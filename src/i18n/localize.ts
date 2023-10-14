193bcd465077348c2ff5ed831eb4c31a51c73110
ElHamster <9559201+ElHamster@users.noreply.github.com>
2023-10-14 12:44:27 +0200

    add support for propper translations and set "en" as default

--- src/i18n/localize.ts
+++ src/i18n/localize.ts
@@ -1,12 +1,21 @@
 import de from "./languages/de.json";
 import en from "./languages/en.json";
 
+const defaultLang = "en";
 const languages: Record<string, any> = { en, de };
 
-export const localize = (key: string, lang = "de") => {
-  try {
-    return key.split(".").reduce((o, i) => o[i], languages[lang]) || key;
-  } catch (e) {
-    return key;
-  }
+export type ILocalizer = (key: string, lang?: string) => string;
+
+export const initLocalize = (initLang: string = defaultLang): ILocalizer => {
+  const localizer = (key: string, lang = initLang): string => {
+    try {
+      return (
+        key.split(".").reduce((o, i) => o?.[i], languages[lang]) ||
+        localizer(key, defaultLang)
+      );
+    } catch (e) {
+      return localizer(key, defaultLang);
+    }
+  };
+  return localizer;
 };
