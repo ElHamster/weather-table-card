be5c91f15774a6c47de88a0d797ef54daee7918e
ElHamster <9559201+ElHamster@users.noreply.github.com>
2023-11-28 06:03:07 +0100

    Add check against endless translation cycle

--- src/i18n/localize.ts
+++ src/i18n/localize.ts
@@ -9,10 +9,18 @@ export type ILocalizer = (key: string, lang?: string) => string;
 export const initLocalize = (initLang: string = defaultLang): ILocalizer => {
   const localizer = (key: string, lang = initLang): string => {
     try {
-      return (
-        key.split(".").reduce((o, i) => o?.[i], languages[lang]) ||
-        localizer(key, defaultLang)
-      );
+      const translation = key
+        .split(".")
+        .reduce((o, i) => o?.[i], languages[lang]);
+
+      // when found, return the correct translation
+      if (translation) return translation;
+
+      // when not found and the language is the default language, return the key so we know what to translate
+      if (lang === defaultLang) return key;
+
+      // when not found and the language is not the default language, try to check the default lang
+      return localizer(key, defaultLang);
     } catch (e) {
       return localizer(key, defaultLang);
     }
